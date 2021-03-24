6db7f632111887659b1d1fa800f96609d4462f70
daikiiin <dannderion@icloud.com>
2021-03-24 20:31:09 +0900

    デバイスのパラメーター許容設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
-=======
->>>>>>> parent of f7c1a55 (Basic認証を導入)
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_kana,:first_name_kana,:birth_day])
+  end
 end
