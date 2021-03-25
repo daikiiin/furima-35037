718d8791d61860c8b0afe19fa5ce5d34d4f19d24
daikiiin <dannderion@icloud.com>
2021-03-26 02:59:38 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_fee_status
+  belongs_to :shipping_fee_destination
+  belongs_to :shipping_fee_day
 end
