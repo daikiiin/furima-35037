d92cc8638a3e6e4f528ec129658a6f90894c8ae1
daikiiin <dannderion@icloud.com>
2021-03-26 16:51:50 +0900

    商品出品ページへの遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
  def index
   @items = Item.all
  end
@@ -7,7 +9,8 @@ class ItemsController < ApplicationController
   @items = Item.new
  end
 
+ private 
  def  item_params
-  params.require(:item).permit(:image).merge(user_id: currrent_user.id)
+  params.require(:item).permit(:name, :explain, :category_id, :item_status_id, :shipping_fee_status_id, :shipping_fee_destination_id, :shipping_fee_day_id, :cost, :image).merge(user_id: currrent_user.id)
  end
 end
