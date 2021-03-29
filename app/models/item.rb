af5f94dced09328d29b801e22a713ae58a1c0c8e
daikiiin <dannderion@icloud.com>
2021-03-29 18:44:19 +0900

    バリデーションの整頓

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,17 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_destination
   belongs_to :shipping_fee_day
 
-  validates :image,                       presence: true
-  validates :name,                        presence: true
-  validates :explain,                     presence: true
-  validates :category_id,                 numericality: { other_than: 1 , message:'を選択して下さい'}
-  validates :item_status_id,              numericality: { other_than: 1 , message:'を選択して下さい'}
-  validates :shipping_fee_status_id,      numericality: { other_than: 1 , message:'を選択して下さい'}
-  validates :shipping_fee_destination_id, numericality: { other_than: 1 , message:'を選択して下さい'}
-  validates :shipping_fee_day_id,         numericality: { other_than: 1 , message:'を選択して下さい'}
+
+  with_options presence: true do
+    validates :image                       
+    validates :name                 
+    validates :explain           
+  end
+  with_options numericality: { other_than: 1 , message:'を選択して下さい'} do
+    validates :category_id         
+    validates :item_status_id      
+    validates :shipping_fee_status_id     
+    validates :shipping_fee_day_id   
+  end   
   validates :cost,                        presence: true, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/}
 end
