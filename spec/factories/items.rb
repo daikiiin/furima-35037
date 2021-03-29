8c5baef7023c35d3e103cb58882e281ae74e9c22
daikiiin <dannderion@icloud.com>
2021-03-29 18:16:13 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name                        {"テスト"}
     explain                     {"テストの説明文を記述する。"}
-    category_id                 {"2"}
-    item_status_id              {"2"}
-    shipping_fee_status_id      {"2"}
-    shipping_fee_destination_id {"2"}
-    shipping_fee_day_id         {"2"}
-    cost                        {"999"}
+    category_id                 {2}
+    item_status_id              {2}
+    shipping_fee_status_id      {2}
+    shipping_fee_destination_id {2}
+    shipping_fee_day_id         {2}
+    cost                        {999}
     association :user
 
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Explain can't be blank")
         end
         it "カテゴリーの情報がなければ投稿できない" do
-          @item.category_id = nil
+          @item.category_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Category を選択して下さい")
         end
         it "商品の状態についての情報がなければ投稿できない" do
-          @item.item_status_id = nil
+          @item.item_status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Item status を選択して下さい")
         end
         it "配送料の負担についての情報がなければ投稿できない" do
-          @item.shipping_fee_status_id = nil
+          @item.shipping_fee_status_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Shipping fee status を選択して下さい")
         end
         it "発送元の地域についての情報がなければ投稿できない" do
-          @item.shipping_fee_destination_id = nil
+          @item.shipping_fee_destination_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Shipping fee destination を選択して下さい")
         end
         it "発送までの日数についての情報がなければ投稿できない" do
-          @item.shipping_fee_day_id = nil
+          @item.shipping_fee_day_id = 1
           @item.valid?
           expect(@item.errors.full_messages).to include("Shipping fee day を選択して下さい")
         end
@@ -63,12 +63,12 @@ RSpec.describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Cost is not a number")
         end
         it "販売価格が300未満だと投稿できない" do
-          @item.cost = "299"
+          @item.cost = 299
           @item.valid?
           expect(@item.errors.full_messages).to include("Cost must be greater than or equal to 300")
         end
         it "販売価格が9999999より大きいと投稿できない" do
-          @item.cost = "10000000"
+          @item.cost = 10000000
           @item.valid?
           expect(@item.errors.full_messages).to include("Cost must be less than or equal to 9999999")
         end
