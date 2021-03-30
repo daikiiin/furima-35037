02a00b010628d4ffe52e75543e568ebe75733a9f
daikiiin <dannderion@icloud.com>
2021-03-30 13:00:18 +0900

    ログインの有無によるページ遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-
+  before_action :authenticate_user!, only: [:new ,:edit]
+  before_action :move_to_index, only: [:edit]
  def index
   @items = Item.all.order(id: "DESC")
  end
@@ -39,5 +39,10 @@ class ItemsController < ApplicationController
 
  def item_params
   params.require(:item).permit(:name, :explain, :category_id, :item_status_id, :shipping_fee_status_id, :shipping_fee_destination_id, :shipping_fee_day_id, :cost, :image).merge(user_id: current_user.id)
+ end
+
+ def move_to_index 
+  @item = Item.find(params[:id])
+  redirect_to root_path if current_user.id != @item.user_id
  end
 end
