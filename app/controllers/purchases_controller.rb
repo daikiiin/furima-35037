f4dfb712c1c0fd8794602679df9bb9ea38e159cb
daikiiin <dannderion@icloud.com>
2021-03-30 18:23:31 +0900

    コントローラーの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,15 @@
 class PurchasesController < ApplicationController
   def index
+    @purchase = Purchase.find(params[:id])
   end
 
   def create
+    @purchase = Purchase.find(params[:id])
+    @purchase.create(purchase_params)
   end
-end
+
+#  private
+#   def purchase_params
+#     params.require(:purchase).permit(:)
+#   end
+# end
