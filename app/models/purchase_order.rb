e645e21f31b5d0b770cdf87bdaaa137952c28d1e
daikiiin <dannderion@icloud.com>
2021-03-31 18:04:08 +0900

    formオブジェクトの修正

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -9,9 +9,10 @@ class PurchaseOrder
      validates :phone_number
    end
      validates :shipping_fee_destination_id, numericality: { other_than: 1 , message:'を選択して下さい'}
-    
+     validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "を正しく入力してください"}
+     validates :phone_number, format: { with: /\A[0-9]+\z/, message: "に数字を使用してください"}
    def save
-     Purchase.create(user_id: user_id, item_id: item_id)
-     Order.create(post_code: post_code, shipping_fee_destination_id: shipping_fee_destination_id, city: city, address: address, building_name: building_name, phone_number: phone_number)
+     purchase = Purchase.create(user_id: user_id, item_id: item_id)
+     Order.create(post_code: post_code, shipping_fee_destination_id: shipping_fee_destination_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
    end
 end
