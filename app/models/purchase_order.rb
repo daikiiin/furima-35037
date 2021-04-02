ba0a1f9e75c413c1eab420f6c6e10ca2b51d0efa
daikiiin <dannderion@icloud.com>
2021-04-02 18:29:16 +0900

    バリデーションの修正

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -8,10 +8,12 @@ class PurchaseOrder
      validates :address
      validates :phone_number
      validates :token
+     validates :user_id
+     validates :item_id
    end
      validates :shipping_fee_destination_id, numericality: { other_than: 1 , message:'を選択して下さい'}
      validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: "を正しく入力してください"}
-     validates :phone_number, format: { with: /\A[0-9]+\z/, message: "に数字を使用してください"}
+     validates :phone_number, format: { with: /\A\d{11}\z/, message: "に数字を使用してください"}
    def save
      purchase = Purchase.create(user_id: user_id, item_id: item_id)
      Order.create(post_code: post_code, shipping_fee_destination_id: shipping_fee_destination_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
